e32a1f79c87da409a54e1eeabda090629b900c9c
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-19 12:53:03 -0300

    Feat: Adding User Avatar routes, controller  and services

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '21e2eqaweqawedawvd|WVD',
+    expiresIn: '1d',
+  },
+};
---
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -0,0 +1,38 @@
+import AppError from "@shared/errors/AppError";
+import { NextFunction, Request, Response } from "express";
+import authConfig from '@config/auth';
+import { verify } from "jsonwebtoken";
+
+interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void{
+  const authHeader = request.headers.authorization;
+
+  if(!authHeader){
+    throw new AppError('JWT Token is missing.');
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  try{
+    const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as ITokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  }catch{
+    throw new AppError('Invalid JWT Token.');
+  }
+};
