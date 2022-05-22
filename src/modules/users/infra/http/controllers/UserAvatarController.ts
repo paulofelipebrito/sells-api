8bb36a033fcb9e27b9b78f987704f24e6fd085c0
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-22 13:21:11 -0300

    Refactoring for DDD

---
+++ src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -0,0 +1,16 @@
+import { instanceToInstance } from 'class-transformer';
+import { Request, Response } from 'express';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
+export default class UserAvatarController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updateAvatar = new UpdateUserAvatarService();
+
+    const user = await updateAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    return response.json(instanceToInstance(user));
+  }
+}
---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,51 @@
+import 'reflect-metadata';
+import 'dotenv/config'
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/infra/typeorm';
+import '@shared/container';
+import uploadConfig from '@config/upload';
+import { pagination } from 'typeorm-pagination';
+import { errors } from 'celebrate';
+import rateLimiter from './middlewares/rateLimiter';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(rateLimiter);
+
+app.use(pagination);
+
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use(errors());
+
+app.use(
+  (
+    error: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction) => {
+      if(error instanceof AppError) {
+        return res.status(error.statusCode).json({
+          status: 'error',
+          message: error.message
+        });
+      }
+
+      return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error'
+      })
+    },
+  );
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333')
+});
