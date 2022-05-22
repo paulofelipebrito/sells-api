fa6ec779896b00e727f9fb448ad0b4b26ecfca9e
Paulo Felipe Brito <paulofelipebrito@hotmail.com>
2022-05-21 21:23:42 -0300

    Feat: Removing access to crypted password in client

--- src/modules/users/controllers/UsersController.ts
+++ src/modules/users/controllers/UsersController.ts
@@ -1,13 +1,15 @@
+import { instanceToInstance } from 'class-transformer';
 import { Request, Response } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUserService';
+
 export default class UsersController {
   public async index(request: Request, response: Response): Promise<Response> {
     const listUser = new ListUserService();
 
     const users = await listUser.execute();
 
-    return response.json((users));
+    return response.json(instanceToInstance(users));
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
@@ -21,6 +23,6 @@ export default class UsersController {
       password,
     });
 
-    return response.json((user));
+    return response.json(instanceToInstance(user));
   }
 }
